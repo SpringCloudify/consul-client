5b69dd58bea79504bb0cffdb9b6cf7688e310178
王佳 <wangjia_1919@163.com>
2017-12-29 14:10:05 +0800

    update package

---
+++ src/main/java/com/springcloudfiy/cloudy/consulclient/controller/DcController.java
@@ -0,0 +1,23 @@
+package com.springcloudfiy.cloudy.consulclient.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author wj42134
+ * @date 2017 12 29 12:51
+ */
+@RestController
+public class DcController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/dc")
+    public String dc() {
+        return "Services: " + discoveryClient.getServices();
+    }
+
+}
